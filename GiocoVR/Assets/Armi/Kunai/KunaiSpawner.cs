4a97014a401366c022d2dda3d7d485e42fb98742
CosimoBovi <cosimo.bovi@gmail.com>
2025-02-13 16:12:38 +0100

    SparoKunai

--- GiocoVR/Assets/Armi/Kunai/KunaiSpawner.cs
+++ GiocoVR/Assets/Armi/Kunai/KunaiSpawner.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class KunaiHandler : MonoBehaviour
+public class KunaiSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // L'asset del proiettile
+    [SerializeField]
+    private GameObject proiettile;
+
+    //Il punto da dove inizio a sparare
+    [SerializeField]
+    private Transform firePoint;
+
+    // L'azione che l'utente fa per saparare
+    [SerializeField]
+    private InputActionReference shootAction;
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        shootAction.action.WasPressedThisFrame() =>
+            se ho premuto in questo frame da come valore vero
+            altrimenti da come valore falso
+        */
+        if(shootAction.action.WasPressedThisFrame()){
+            /*
+            Instantiate => Crea
+            Crea il proiettile nella posizione firePoint.position, con rotazione
+            firePoint.rotation
+            */
+            Instantiate(proiettile, firePoint.position, firePoint.rotation);
+        }
     }
 }
